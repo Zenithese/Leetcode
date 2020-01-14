e39ed0e00e3799e1de3bfc4b128aaf4968848659
Justin <jandersen770@gmail.com>
2020-01-14 14:15:27 -0500

    backtracking

---
+++ 22.js
@@ -0,0 +1,21 @@
+var generateParenthesis = function (n) {
+    let permutations = [];
+    
+    var backtrack = function (perm, left, right) {
+        if (perm.length === n * 2){
+            permutations.push(perm);
+            return;
+        }
+        if (left < n){
+            backtrack(perm + '(', left + 1, right);
+        }
+        if (right < left){
+            backtrack(perm + ')', left, right + 1);
+        }
+    }
+
+    backtrack('', 0, 0);
+    return permutations;
+};
+
+console.log(generateParenthesis(3));
