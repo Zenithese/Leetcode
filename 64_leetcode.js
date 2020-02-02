b74c30e4ced2289c00de97818a83979bcde45beb
Justin <jandersen770@gmail.com>
2020-02-01 20:10:10 -0500

    99.10% runtime, 100% space

--- 64_leetcode.js
+++ 64_leetcode.js
@@ -1,5 +1,32 @@
 // https://leetcode.com/problems/minimum-path-sum/
 
 function minPathSum(grid) {
+    let m = grid.length - 1;
+    let n = grid[0].length - 1;
+
+    for (let i = 1; i < grid[0].length; i++) {
+        grid[0][i] = grid[0][i - 1] + grid[0][i];
+    }
+
+    for (let i = 1; i < grid.length; i++) {
+        grid[i][0] = grid[i][0] + grid[i - 1][0];
+    }
+
+    for (let i = 1; i < grid.length; i++) {
+        for (let j = 1; j < grid[i].length; j++) {
+            if (grid[i - 1][j] < grid[i][j - 1]) {
+                grid[i][j] = grid[i - 1][j] + grid[i][j];
+            } else {
+                grid[i][j] = grid[i][j - 1] + grid[i][j]
+            }
+        }
+    }
     
+    return grid[m][n];
 }
+input = [
+    [1, 3, 1],
+    [1, 5, 1],
+    [4, 2, 1]
+];
+console.log(minPathSum(input));
