7aa8e25b90e5e556d0331ec7f304003256005277
jandersen770 <jandersen770@gmail.com>
2020-07-10 18:16:20 -0400

    update

--- google interview card/sorts/Find First and Last Position of Element in Sorted Array.js
+++ google interview card/sorts/Find First and Last Position of Element in Sorted Array.js
@@ -1,3 +1,9 @@
+// Given an array of integers nums sorted in ascending order, find the starting and ending position of a given target value.
+
+// Your algorithm's runtime complexity must be in the order of O(log n).
+
+// If the target is not found in the array, return [-1, -1].
+
 var searchRange = function (nums, target) {
 
     let start = -1, end = -1
