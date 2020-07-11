9ebf6f7edfd7fa5e8802c807681f1bb8dc6bd3e8
jandersen770 <jandersen770@gmail.com>
2020-07-11 15:46:26 -0400

    update

--- google interview card/Dynamic Programming/Longest Palindromic Substring.js
+++ google interview card/Dynamic Programming/Longest Palindromic Substring.js
@@ -4,9 +4,13 @@ var longestPalindrome = function (s) {
 
     let longest = ""
 
+    let count = 0
+
     const palindrome = (start, end) => {
 
         while ((start - 1 >= 0 && end + 1 < s.length) && (s[start - 1] === s[end + 1])) {
+
+            count++
 
             start--
 
@@ -19,6 +23,8 @@ var longestPalindrome = function (s) {
     }
 
     for (let i = 0; i < s.length; i++) {
+
+        count++
 
         let oddCurrent = palindrome(i, i)
 
@@ -34,6 +40,130 @@ var longestPalindrome = function (s) {
 
     }
 
+    console.log(count)
+
     return longest
 
 };
+
+var manachersAlgorithm = (s) => {
+
+    if (s.length <= 1) return s
+
+    s = "|" + s.split("").join("|") + "|"
+    let longest = [0, ""]
+    let dp = new Array(s.length).fill(0)
+    let center = 0
+    let end = 0
+    let count = 0
+
+
+
+    for (let i = 0; i < s.length; i++) {
+
+        count++
+
+        let mirror = (center * 2) - i
+
+        if (i < end) dp[i] = Math.min(end - i, dp[mirror])
+
+
+
+        a = i + (1 + dp[i])
+
+        b = i - (1 + dp[i])
+
+        while ((b >= 0 && a + 1 < s.length) && (s[b] === s[a])) {
+
+            dp[i]++
+
+
+
+            a++
+
+            b--
+
+            count++
+
+        }
+
+    
+
+        if (dp[i] >= longest[0]) longest = [dp[i], current = s.slice(i - dp[i], i + dp[i] + 1)]
+
+        if (i + dp[i] > end) {
+
+            center = i
+
+            end = i + dp[i]
+
+        }
+        
+    }
+
+    console.log(count)
+
+    return [longest[1].split("|").join(""), dp]
+
+}
+
+console.log("O(n^2):", longestPalindrome("abaxabaxabb"))
+// console.log("O(n):", manachersAlgorithm("aaaaaaaaaaa"))
+console.log("O(n):", manachersAlgorithm("abaxabaxabb"))
+// console.log("O(n):", manachersAlgorithm("cbba"))
+// console.log("O(n):", manachersAlgorithm("aaaa"))
+// console.log("O(n):", manachersAlgorithm("aaaabaaa"))
+// console.log("O(n):", manachersAlgorithm("ababababa"))
+
+
+// s = "|" + s.split("").join("|") + "|"
+
+// console.log(s)
+
+// let longest = ""
+
+// let dp = new Array(s.length).fill(0)
+
+// const palindrome = (start, end) => {
+
+//     while ((start - 1 >= 0 && end + 1 < s.length) && (s[start - 1] === s[end + 1])) {
+
+//         start--
+
+//         end++
+
+//         dp[end] = dp[start]
+
+//     }
+
+//     let current = s.slice(start, end + 1)
+
+//     longest = current.length > longest.length ? current : longest
+
+//     return current.length
+
+// }
+
+// let center = 0
+// let start = 0, end = 0
+
+// for (let i = 0; i < s.length; i++) {
+
+//     dp[i] = palindrome(start, end)
+
+//     quotient = Math.floor(dp[i] / 4)
+
+//     i += quotient <= 1 ? 0 : quotient % 2 === 1 && dp[i] % 2 === 0 ? quotient - 1 : quotient
+
+//     // i += quotient % 2 === 0 ? quotient + 1 : quotient + 2
+
+//     start = i - quotient
+
+//     end = i + quotient
+
+//     console.log(start, i, end)
+
+// }
+
+// return [longest.split("|").join(""), dp]
+
