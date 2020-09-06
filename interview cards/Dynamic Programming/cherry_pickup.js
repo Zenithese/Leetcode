2e858d5097d21f0a76a1050799dacdc5f7af13b6
jandersen770 <jandersen770@gmail.com>
2020-09-06 17:08:39 -0400

    update

--- interview cards/Dynamic Programming/cherry_pickup.js
+++ interview cards/Dynamic Programming/cherry_pickup.js
@@ -56,12 +56,60 @@ var cherryPickup = function (grid, returning = false, i = 0, j = 0, memo = new A
 }
 
 // console.log(cherryPickup(grid))
-console.log(cherryPickup([
-    [1, 1, 1, 1, 0, 0, 0], 
-    [0, 0, 0, 1, 0, 0, 0], 
-    [0, 0, 0, 1, 0, 0, 1], 
-    [1, 0, 0, 1, 0, 0, 0], 
-    [0, 0, 0, 1, 0, 0, 0], 
-    [0, 0, 0, 1, 0, 0, 0], 
+// console.log(cherryPickup([
+//     [1, 1, 1, 1, 0, 0, 0],
+//     [0, 0, 0, 1, 0, 0, 0],
+//     [0, 0, 0, 1, 0, 0, 1],
+//     [1, 0, 0, 1, 0, 0, 0],
+//     [0, 0, 0, 1, 0, 0, 0],
+//     [0, 0, 0, 1, 0, 0, 0],
+//     [0, 0, 0, 1, 1, 1, 1]
+// ]))
+
+
+var cherryPickup2 = function (grid) {
+    const N = grid.length;
+    let dp = new Array(N).fill(null).map(() =>
+        new Array(N).fill(null).map(() =>
+            new Array(N).fill(null)))
+
+    /**
+     * @param {Number} Ar row for picker A
+     * @param {Number} Ac column for picker A
+     * @param {Number} Br row for picker B
+     */
+    let dfs = function (Ar, Ac, Br) {
+        let Bc = Ar + Ac - Br;
+
+        if (Ar >= N || Ac >= N || Br >= N || Bc >= N) return -1; // out of boundary
+        if (grid[Ar][Ac] < 0 || grid[Br][Bc] < 0) return -1; // meet a thorn
+        if ((Ar == N - 1 && Ac == N - 1) || (Br == N - 1 && Bc == N - 1)) return grid[N - 1][N - 1]; // reaches to the end
+        if (dp[Ar][Ac][Br] != null) return dp[Ar][Ac][Br]; // memoization
+
+        let curPickup = (Ar == Br && Ac == Bc) ? grid[Ar][Ac] : (grid[Ar][Ac] + grid[Br][Bc]);
+        let remainPickUp = Math.max(dfs(Ar + 1, Ac, Br + 1), // A down, B down
+            dfs(Ar + 1, Ac, Br),   // A down, B right
+            dfs(Ar, Ac + 1, Br + 1), // A right, B down
+            dfs(Ar, Ac + 1, Br))   // A right, B right
+
+        if (remainPickUp < 0) return dp[Ar][Ac][Br] = -1; // no available path
+
+        remainPickUp += curPickup;
+        dp[Ar][Ac][Br] = remainPickUp;
+        if (Ar === 0 && Ac === 0 && Br === 0) console.log(dp)
+        return dp[Ar][Ac][Br]
+        
+    }
+
+    return Math.max(0, dfs(0, 0, 0));
+};
+
+console.log(cherryPickup2([
+    [1, 1, 1, 1, 0, 0, 0],
+    [0, 0, 0, 1, 0, 0, 0],
+    [0, 0, 0, 1, 0, 0, 1],
+    [1, 0, 0, 1, 0, 0, 0],
+    [0, 0, 0, 1, 0, 0, 0],
+    [0, 0, 0, 1, 0, 0, 0],
     [0, 0, 0, 1, 1, 1, 1]
 ]))
---
+++ interview cards/Dynamic Programming/russian_doll.py
@@ -0,0 +1,3 @@
+def maxEnvelopes(self, envelopes: List[List[int]]) -> int:
+
+    for i in range(len(envelopes)): 
