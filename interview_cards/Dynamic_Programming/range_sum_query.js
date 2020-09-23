b0509e905a3b6c22dd9c5aecd8e52e4ed3363406
jandersen770 <jandersen770@gmail.com>
2020-09-23 12:06:03 -0400

    runtime: 99.57 faster

--- interview_cards/Dynamic_Programming/range_sum_query.js
+++ interview_cards/Dynamic_Programming/range_sum_query.js
@@ -1,6 +1,7 @@
 // Given a 2D matrix matrix, 
 // find the sum of the elements inside the rectangle defined by its upper left corner (row1, col1) 
 // and lower right corner (row2, col2).
+
 class NumMatrix {
     constructor(matrix) {
         this.data = matrix
@@ -18,7 +19,44 @@ class NumMatrix {
         return sum
 
     }
+
+    dP(row1, col1, row2, col2) {
+
+        let dp = new Array(this.data.length + 1).fill(0).map(row => new Array(this.data[0].length + 1).fill(0))
+
+        for (let row = 1; row < dp.length; row++) {
+            for (let col = 1; col < dp[0].length; col++) {
+                dp[row][col] = this.data[row - 1][col - 1] + dp[row][col - 1] + dp[row - 1][col] - dp[row - 1][col - 1]
+            }
+        }
+
+        return dp[row2 + 1][col2 + 1] - dp[row1][col2 + 1] - dp[row2 + 1][col1] + dp[row1][col1]
+
+    }
+
 }
+
+class DpNumMatrix {
+    constructor(matrix) {
+        if (matrix == null || matrix.length === 0 || matrix[0].length === 0) return;
+        this.data = matrix
+        this.dp = new Array(this.data.length + 1).fill(0).map(row => new Array(this.data[0].length + 1).fill(0))
+
+        for (let row = 1; row < this.dp.length; row++) {
+            for (let col = 1; col < this.dp[0].length; col++) {
+                this.dp[row][col] = this.data[row - 1][col - 1] + this.dp[row][col - 1] + this.dp[row - 1][col] - this.dp[row - 1][col - 1]
+            }
+        }
+
+    }
+
+    sumRegion(row1, col1, row2, col2) {
+
+        return this.dp[row2 + 1][col2 + 1] - this.dp[row1][col2 + 1] - this.dp[row2 + 1][col1] + this.dp[row1][col1]
+
+    }
+} // Runtime: 92 ms, faster than 99.57% of JavaScript online submissions for Range Sum Query 2D - Immutable.
+  // Memory Usage: 40.6 MB, less than 64.78 % of JavaScript online submissions for Range Sum Query 2D - Immutable.
 
 let matrix = [
     [3, 0, 1, 4, 2],
@@ -28,4 +66,6 @@ let matrix = [
     [1, 0, 3, 0, 5]
 ]
 
-console.log(new NumMatrix(matrix).sumRegion(2, 1, 4, 3)) // => 8
+// console.log(new NumMatrix(matrix).sumRegion(2, 1, 4, 3)) // => 8
+console.log(new NumMatrix(matrix).sumRegion(2, 1, 4, 3))
+console.log(new DpNumMatrix(matrix).sumRegion(2, 1, 4, 3))
