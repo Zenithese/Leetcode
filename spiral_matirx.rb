ab61f69ddc6ca624d4e4932ab8835eb9db3344e9
jandersen770 <jandersen770@gmail.com>
2020-10-14 16:58:10 -0400

    Runtime: 56 ms, faster than 26.25% of Ruby online submissions for Spiral Matrix. Memory Usage: 209.8 MB, less than 17.50% of Ruby online submissions for Spiral Matrix.

---
+++ spiral_matirx.rb
@@ -0,0 +1,32 @@
+# https://leetcode.com/problems/spiral-matrix/submissions/
+
+# Runtime: 56 ms, faster than 26.25% of Ruby online submissions for Spiral Matrix.
+# Memory Usage: 209.8 MB, less than 17.50% of Ruby online submissions for Spiral Matrix.
+
+require 'set'
+
+def spiral_order(matrix, spiral = [], visited = Set.new)
+
+    row, col = 0, 0
+    direction, directions = 0, [[0, 1], [1, 0], [0, -1], [-1, 0]]
+
+    if matrix.length > 0
+        while spiral.length < matrix.length * matrix[0].length
+            if !visited.include?("#{row}-#{col}")
+                spiral << matrix[row][col]
+                visited << "#{row}-#{col}"
+            end
+
+            direction = (direction + 1) % 4 if (row == matrix.length - 1 && direction == 1) || (col == matrix[0].length - 1 && direction == 0) || (row == 0 && direction == 3) || (col == 0 && direction == 2) || visited.include?("#{row + directions[direction][0]}-#{col + directions[direction][1]}")
+            row += directions[direction][0]
+            col += directions[direction][1]
+        end
+    end
+
+    spiral
+
+end
+
+p spiral_order([[1,2,3],
+                [4,5,6],
+                [7,8,9]])
