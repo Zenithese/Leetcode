b61ca47c07cc7ca87c8b8434bb1d0a58149e4a65
jandersen770 <jandersen770@gmail.com>
2020-10-14 17:05:23 -0400

    Runtime: 40 ms, faster than 76.25% of Ruby online submissions for Spiral Matrix. Memory Usage: 209.9 MB, less than 17.50% of Ruby online submissions for Spiral Matrix.

--- spiral_matirx.rb
+++ spiral_matirx.rb
@@ -11,13 +11,14 @@ def spiral_order(matrix, spiral = [], visited = Set.new)
     direction, directions = 0, [[0, 1], [1, 0], [0, -1], [-1, 0]]
 
     if matrix.length > 0
-        while spiral.length < matrix.length * matrix[0].length
+        m, n = matrix.length, matrix[0].length
+        while spiral.length < m * n
             if !visited.include?("#{row}-#{col}")
                 spiral << matrix[row][col]
                 visited << "#{row}-#{col}"
             end
 
-            direction = (direction + 1) % 4 if (row == matrix.length - 1 && direction == 1) || (col == matrix[0].length - 1 && direction == 0) || (row == 0 && direction == 3) || (col == 0 && direction == 2) || visited.include?("#{row + directions[direction][0]}-#{col + directions[direction][1]}")
+            direction = (direction + 1) % 4 if (row == m - 1 && direction == 1) || (col == n - 1 && direction == 0) || (row == 0 && direction == 3) || (col == 0 && direction == 2) || visited.include?("#{row + directions[direction][0]}-#{col + directions[direction][1]}")
             row += directions[direction][0]
             col += directions[direction][1]
         end
