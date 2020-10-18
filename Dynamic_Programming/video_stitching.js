b2095e60bf9c914e24e0c3b24f753a0040e63bfa
jandersen770 <jandersen770@gmail.com>
2020-10-18 00:44:57 -0400

    Runtime: 76 ms, faster than 75.00 % of JavaScript online submissions for Video Stitching. Memory Usage: 38.5 MB, less than 46.43 % of JavaScript online submissions for Video Stitching.

--- Dynamic_Programming/video_stitching.js
+++ Dynamic_Programming/video_stitching.js
@@ -1,8 +1,5 @@
 // https://leetcode.com/problems/video-stitching/
 
-
-
-
 var memoVideoStitching = function (clips, T, t = 0, memo = {}, stitch = []) {
 
     let key = `${clips}-${t}`
@@ -23,16 +20,24 @@ var memoVideoStitching = function (clips, T, t = 0, memo = {}, stitch = []) {
 
 };
 
+// Runtime: 76 ms, faster than 75.00 % of JavaScript online submissions for Video Stitching.
+// Memory Usage: 38.5 MB, less than 46.43 % of JavaScript online submissions for Video Stitching.
+
 var videoStitching = (clips, T, shots = []) => {
 
     for (let i = 0; i < clips.length; i++) {
         if (!shots[clips[i][0]] || clips[i][1] > shots[clips[i][0]][1]) shots[clips[i][0]] = clips[i]
-    }
+    } // pick clips by start time and max end time
 
-    let coverage = [shots[0]]; last = shots[0]
+    if (shots[0] && shots[0][1] >= T) return 1
+    if (!shots[0]) return -1
+
+    let coverage = [shots[0]]; last = shots[0];
     for (let i = 1; i < shots.length; i++) {
         if (!shots[i]) {
             continue
+        } else if (coverage[coverage.length - 1][1] >= T || last[1] >= T) {
+            break
         } else if (shots[i][0] <= coverage[coverage.length - 1][1] && last[1] < shots[i][1]) {
             last = shots[i]
         } else if (shots[i][0] > coverage[coverage.length - 1][1] && last) {
@@ -40,9 +45,13 @@ var videoStitching = (clips, T, shots = []) => {
         }
     }
 
-    return last !== coverage[coverage.length - 1] ? coverage.length + 1 : coverage.length
+    if (last !== coverage[coverage.length - 1]) coverage.push(last)
+    if (coverage[coverage.length - 1][1] < T) return -1
+    return coverage.length
 
 }
-const clips = [[0, 2], [4, 6], [8, 10], [1, 9], [1, 5], [5, 9]], T = 10
+
+const clips = [[0, 1], [1, 2]], T = 5
+// const clips = [[0, 2], [4, 6], [8, 10], [1, 9], [1, 5], [5, 9]], T = 10
 // const clips = [[0, 5], [1, 6], [2, 7], [3, 8], [4, 9], [5, 10], [6, 11], [7, 12], [8, 13], [9, 14], [10, 15], [11, 16], [12, 17], [13, 18], [14, 19], [15, 20], [16, 21], [17, 22], [18, 23], [19, 24], [20, 25], [21, 26], [22, 27], [23, 28], [24, 29], [25, 30], [26, 31], [27, 32], [28, 33], [29, 34], [30, 35], [31, 36], [32, 37], [33, 38], [34, 39], [35, 40], [36, 41], [37, 42], [38, 43], [39, 44], [40, 45], [41, 46], [42, 47], [43, 48], [44, 49], [45, 50], [46, 51], [47, 52], [48, 53], [49, 54]], T = 50
 console.log(videoStitching(clips, T)) // => 50
