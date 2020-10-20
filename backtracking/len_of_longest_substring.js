9e75e710fec6daa655dcde0b7e7ab9da2e115386
jandersen770 <jandersen770@gmail.com>
2020-10-20 15:28:53 -0400

    Runtime: 584 ms, faster than 6.74% of JavaScript online submissions for Longest Substring Without Repeating Characters. Memory Usage: 54.6 MB, less than 5.69% of JavaScript online submissions for Longest Substring Without Repeating Characters.

---
+++ backtracking/len_of_longest_substring.js
@@ -0,0 +1,25 @@
+// https://leetcode.com/problems/longest-substring-without-repeating-characters/submissions/
+
+// Runtime: 584 ms, faster than 6.74 % of JavaScript online submissions for Longest Substring Without Repeating Characters.
+// Memory Usage: 54.6 MB, less than 5.69 % of JavaScript online submissions for Longest Substring Without Repeating Characters.
+
+var lengthOfLongestSubstring = function (s) {
+
+    let substrings = [], substring = "", backtrack = {}
+
+    for (let i = 0; i < s.length; i++) {
+        if (!substring.includes(s[i])) {
+            substring += s[i]
+            backtrack[s[i]] = i + 1
+        } else {
+            substrings.push(substring.length)
+            backtrack = {
+                [substring = s[backtrack[s[i]]]]: (i = backtrack[s[i]]) + 1
+            }
+        }
+    }
+
+    substrings.push(substring.length)
+    return Math.max(...substrings)
+
+};
