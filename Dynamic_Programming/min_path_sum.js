aa90235f90ca09ec26f7ba87128a5dceb1c2d73e
jandersen770 <jandersen770@gmail.com>
2020-10-21 12:19:15 -0400

    Runtime: 68 ms, faster than 99.11 % of JavaScript online submissions for Triangle. Memory Usage: 39.3 MB, less than 6.83 % of JavaScript online submissions for Triangle.

--- Dynamic_Programming/min_path_sum.js
+++ Dynamic_Programming/min_path_sum.js
@@ -1,17 +1,17 @@
 // https://leetcode.com/problems/triangle/submissions/
 
-// Runtime: 84 ms, faster than 42.73 % of JavaScript online submissions for Triangle.
-// Memory Usage: 42.6 MB, less than 6.83 % of JavaScript online submissions for Triangle.
+// Runtime: 68 ms, faster than 99.11 % of JavaScript online submissions for Triangle.
+// Memory Usage: 39.3 MB, less than 6.83 % of JavaScript online submissions for Triangle.
 
-var minimumTotal = function (triangle, memo = {}, i = 0, j = 0, layer = triangle[i], key = `${i}-${j}`) {
+var minimumTotal = function (triangle, memo = triangle.map(layer => layer.slice().fill(undefined)), i = 0, j = 0) {
 
-    if (key in memo) return memo[key]; if (i >= triangle.length) return 0
+    if (i >= triangle.length) return 0; if (memo[i][j]) return memo[i][j];
 
-    const right = Math.min(j + 1, layer.length - 1)
+    const right = Math.min(j + 1, triangle[i].length - 1)
 
-    return memo[key] = Math.min(
-        layer[j] + minimumTotal(triangle, memo, i + 1, j),
-        layer[right] + minimumTotal(triangle, memo, i + 1, right)
+    return memo[i][j] = Math.min(
+        triangle[i][j] + minimumTotal(triangle, memo, i + 1, j),
+        triangle[i][right] + minimumTotal(triangle, memo, i + 1, right)
     )
 
 };
