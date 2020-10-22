185af585087c2b07cdbc91dfa3b40feb91545c2e
jandersen770 <jandersen770@gmail.com>
2020-10-22 11:44:30 -0400

    Update

--- fetch.js
+++ fetch.js
@@ -1,11 +1,11 @@
 const fetch = require("node-fetch");
 
-// async function get() {
-//     const response = await fetch("https://jsonplaceholder.typicode.com/users/")
-//     return response.json()
-// }
+async function get() {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users/")
+    return response.json()
+}
 
-// get().then(res => console.log(res.sort((a, b) => (a.name < b.name))))
+get().then(res => console.log(res.sort((a, b) => (a.name < b.name))))
 
 (async function () {
     const response = await fetch("https://jsonplaceholder.typicode.com/users/3");
@@ -15,3 +15,21 @@ const fetch = require("node-fetch");
 })().then(res => {
     console.log(`${res.name} works for ${res.company.name}`)
 }).catch(e => console.log(e.message))
+
+(async function () {
+    const [person1, person2] = await Promise.all([
+        fetch("https://jsonplaceholder.typicode.com/users/1"),
+        fetch("https://jsonplaceholder.typicode.com/users/2")
+    ])
+
+    if (!person1.ok || !person2.ok) {
+        throw new Error(`person1:${person1.status}, person2:${person2.status}`)
+    }
+
+    return {
+        res1: await person1.json(),
+        res2: await person2.json()
+    }
+})().then(({ res1, res2 }) => {
+    console.log([res1, res2]);
+}).catch(err => console.log(err))
