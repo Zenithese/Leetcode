effee8693bf9a9c7c2d69e5cf9953d397aeff4f8
jandersen770 <jandersen770@gmail.com>
2020-11-02 16:20:09 -0500

    Update

---
+++ unique_BST.rb
@@ -0,0 +1,62 @@
+class BST
+   attr_accessor :value, :left, :right
+    
+    def initialize val
+        @value = val
+        @left = nil
+        @right = nil
+    end
+end
+
+def num_trees(n)
+    
+    trees = Array.new(n).each_with_index.map { |n, i| i + 1 }.permutation.to_a
+    
+    trees = trees.each_with_index.map { |tree| make_tree(tree) }
+        
+    trees = trees.select { |tree| n == count_nodes(tree) }
+
+    trees.each { |tree| p tree }
+    
+end
+
+def make_tree(tree)
+
+    def contruct(node, value)
+        if(value > node.value)
+            if(node.right)
+                contruct(node.right, value)
+            else
+                node.right = BST.new(value)
+            end
+        else
+            if(node.left)
+                contruct(node.left, value)
+            else
+                node.left = BST.new(value)
+            end
+        end
+    end
+
+    root = BST.new(tree.shift);
+    tree.each{|e| contruct(root, e) }
+    root
+
+end
+
+def count_nodes(tree)
+    stack = [tree]
+    count = 0
+
+    while stack.length > 0
+        count += 1
+        node = stack.pop()
+        
+        stack.push(node.right) if node.right != nil
+        stack.push(node.left) if node.left != nil
+    end
+
+    count
+end
+
+p num_trees(3)
