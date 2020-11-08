ffdbd8469e26b2caacda34505d1cb60b8a8e2e94
jandersen770 <jandersen770@gmail.com>
2020-11-08 15:22:05 -0500

    Runtime: 88 ms, faster than 60.00% of JavaScript online submissions for Interleaving String. Memory Usage: 41.2 MB, less than 10.56% of JavaScript online submissions for Interleaving String.

--- interleaving_string.js
+++ interleaving_string.js
@@ -1,12 +1,33 @@
 // https://leetcode.com/problems/interleaving-string/
 
-var isInterleave = function (s1, s2, s3) {
+var isInterleave = (s1, s2, s3) => {
+    console.log("recursive:", recursiveIsInterleave(s1, s2, s3))
+    console.log("memo:", memoIsInterleave(s1, s2, s3))
+    console.log("dp:",  dpIsInterleave(s1, s2, s3))
+}
 
-    dp = [...Array(s1.length)].map(_ => new Array)
+// dp Runtime: 88 ms, faster than 60.00 % of JavaScript online submissions for Interleaving String.
+// dp Memory Usage: 41.2 MB, less than 10.56 % of JavaScript online submissions for Interleaving String.
 
-    for (let i = 0; i < s1.length; i++) {
-        for (let j = 0; j < s2.length; j++) {
-            // logic
+var dpIsInterleave = function (s1, s2, s3) {
+
+    if (s1.length + s2.length !== s3.length) return false
+
+    const dp = [...Array(s1.length + 1)].map(_ => new Array)
+
+    for (let i = 0; i <= s1.length; i++) {
+        for (let j = 0; j <= s2.length; j++) {
+
+            if (i === 0 && j === 0) {
+                dp[i][j] = true
+            } else if (i === 0) {
+                dp[i][j] = dp[i][j - 1] && s2[j - 1] === s3[i + j - 1]
+            } else if (j === 0) {
+                dp[i][j] = dp[i - 1][j] && s1[i - 1] === s3[i + j - 1]
+            } else {
+                dp[i][j] = (dp[i][j - 1] && s2[j - 1] === s3[i + j - 1]) || (dp[i - 1][j] && s1[i - 1] === s3[i + j - 1])
+            }
+            
         }
     }
 
@@ -63,7 +84,5 @@ var anotherMemoIsInterleave = function (s1, s2, s3, i = 0, j = 0, curr = "", mem
 
 };
 
-let s1 = "aabcc", s2 = "dbbca", s3 = "aadbbcbcac"
-console.log(isInterleave(s1, s2, s3))
-console.log(recursiveIsInterleave(s1, s2, s3))
-console.log(memoIsInterleave(s1, s2, s3))
+const s1 = "aabcc", s2 = "dbbca", s3 = "aadbbcbcac"
+isInterleave(s1, s2, s3)
