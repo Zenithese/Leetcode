dade11760f3da37214cf914e7475b9ad5c586882
jandersen770 <jandersen770@gmail.com>
2021-11-05 09:41:40 -0700

    var combinationSum = function(candidates, target, res = []) {
        const dfs = (target, idx, path) => {
            if (target === 0) return res.push([...path]);
            for (let i = idx; i < candidates.length; i++) {
                if (candidates[i] > target) return;
                path.push(candidates[i])
                dfs(target - candidates[i], i, path)
                path.pop()
            }
        }
    
        candidates.sort((a, b) => a - b)
        dfs(target, 0, [])
        return res
    
    };

--- combinationSum.js
+++ combinationSum.js
@@ -1,17 +1,20 @@
+// Runtime: 88 ms, faster than 83.53 % of JavaScript online submissions for Combination Sum.
+// Memory Usage: 41.1 MB, less than 92.18 % of JavaScript online submissions for Combination Sum.
+
 var combinationSum = function (candidates, target, res = []) {
 
-    const dfs = (target, idx, res, path) => {
+    const dfs = (target, idx, path) => {
         if (target === 0) return res.push([...path]);
         for (let i = idx; i < candidates.length; i++) {
             if (candidates[i] > target) return;
             path.push(candidates[i])
-            dfs(target - candidates[i], i, res, path)
+            dfs(target - candidates[i], i, path)
             path.pop()
         }
     }
 
     candidates.sort((a, b) => a - b)
-    dfs(target, 0, res, [])
+    dfs(target, 0, [])
     return res
 
 };
