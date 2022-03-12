d8fbf9be69d26e25db10f78f09b113c94c976466
Zenithese <jandersen770@gmail.com>
2022-03-12 13:16:46 -0800

    dp can jump 2

--- canJump2.js
+++ canJump2.js
@@ -14,3 +14,18 @@ var jump = function (nums, i = 0, memo = {}) {
 
     return memo[i] = Math.min(...attemps);
 };
+
+// Runtime: 178 ms, faster than 37.98 % of JavaScript online submissions for Jump Game II.
+// Memory Usage: 44.1 MB, less than 57.52 % of JavaScript online submissions for Jump Game II.
+
+var jump = function (nums) {
+    const dp = new Array(nums.length).fill(Infinity); dp[0] = 0;
+
+    nums.forEach((num, i) => {
+        for (let j = i + 1; j <= i + num && j < nums.length; j++) {
+            dp[j] = Math.min(dp[j], dp[i] + 1);
+        };
+    });
+
+    return dp[nums.length - 1];
+};
