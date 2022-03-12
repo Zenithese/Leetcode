ad7414696c348822cd7007d567a83abdb09e99cd
Zenithese <jandersen770@gmail.com>
2022-03-11 17:14:06 -0800

    add closing curly bracket

--- Dynamic_Programming/isValid.js
+++ Dynamic_Programming/isValid.js
@@ -1,27 +1,27 @@
 // Runtime: 70 ms, faster than 80.41 % of JavaScript online submissions for Valid Parentheses.
 // Memory Usage: 47.9 MB, less than 5.29 % of JavaScript online submissions for Valid Parentheses.
 
-var isValid = function (s) {
+var isValid = function(s) {
     if (s.length & 1) return false;
-
+    
     for (i = 0; i < s.length; i++) {
-
+        
         if (s[i] === ')' && s[i - 1] === '(') {
             s = s.slice(0, i - 1) + s.slice(i + 1, s.length)
             i -= 3
         }
-
+        
         if (s[i] === '}' && s[i - 1] === '{') {
             s = s.slice(0, i - 1) + s.slice(i + 1, s.length)
             i -= 3
         }
-
+        
         if (s[i] === ']' && s[i - 1] === '[') {
             s = s.slice(0, i - 1) + s.slice(i + 1, s.length)
             i -= 3
-        }
-
+        }    
+        
     }
-
+    
     return !Boolean(s.length)
 };
